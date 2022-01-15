1cfa492bae02293fad2e046e20e99c977a74f55c
Tuna Cinsoy <tunacinsoy@gmail.com>
2022-01-15 18:29:48 +0300

    Create Program.cs

---
+++ az204-redis/Program.cs
@@ -0,0 +1,30 @@
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static string connectionString = "az204redis21202.redis.cache.windows.net:6380,password=7lO6XPzYgMg6qMQ93vQPgjlAlI0PRUOVaAzCaHBN1I8=,ssl=True,abortConnect=False";
+
+    static async Task Main(string[] args)
+    {
+        // The connection to the Azure Cache for Redis is managed by the ConnectionMultiplexer class.
+        using (var cache = ConnectionMultiplexer.Connect(connectionString))
+        {
+            IDatabase db = cache.GetDatabase();
+
+            // Snippet below executes a PING to test the server connection
+            var result = await db.ExecuteAsync("ping");
+            Console.WriteLine($"PING = {result.Type} : {result}");
+
+            // Call StringSetAsync on the IDatabase object to set the key "test:key" to the value "100"
+            bool setValue = await db.StringSetAsync("test:key", "100");
+            Console.WriteLine($"SET: {setValue}");
+
+            // StringGetAsync takes the key to retrieve and return the value
+            string getValue = await db.StringGetAsync("test:key");
+            Console.WriteLine($"GET: {getValue}");
+
+        }
+    }
+
+}
